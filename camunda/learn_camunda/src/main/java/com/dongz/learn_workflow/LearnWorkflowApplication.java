99a5c7563d724ec1b9a3a92476dfda0089bed73d
dongz <89518029@qq.com>
2020-05-19 01:04:12 +0800

    camunda init

---
+++ camunda/learn_camunda/src/main/java/com/dongz/learn_workflow/LearnWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.dongz.learn_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWorkflowApplication.class, args);
+    }
+
+}
---
+++ camunda/learn_camunda/src/test/java/com/dongz/learn_workflow/LearnWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongz.learn_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
