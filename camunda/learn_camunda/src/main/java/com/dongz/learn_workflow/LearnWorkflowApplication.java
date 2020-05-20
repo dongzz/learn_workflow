9dc288cb7bf5609659556e038827e4dca5e3436c
dongz <89518029@qq.com>
2020-05-20 10:25:52 +0800

    camunda init

--- camunda/learn_camunda/src/main/java/com/dongz/learn_workflow/LearnWorkflowApplication.java
+++ camunda/learn_camunda/src/main/java/com/dongz/learn_workflow/LearnWorkflowApplication.java
@@ -1,8 +1,10 @@
 package com.dongz.learn_workflow;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableProcessApplication
 @SpringBootApplication
 public class LearnWorkflowApplication {
 
